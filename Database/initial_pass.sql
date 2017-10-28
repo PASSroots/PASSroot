2855164631353df710fe43964c75136338591476
HuYuHsiang <nightlord851108@gmail.com>
2017-10-28 16:27:04 +0800

    Create course table in sql

--- Database/initial_pass.sql
+++ Database/initial_pass.sql
@@ -13,10 +13,28 @@
 
 
 -- 導出 pass 的資料庫結構
+DROP DATABASE IF EXISTS `pass`;
 CREATE DATABASE IF NOT EXISTS `pass` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `pass`;
 
+-- 導出  表 pass.courses 結構
+DROP TABLE IF EXISTS `courses`;
+CREATE TABLE IF NOT EXISTS `courses` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `course_id` varchar(6) NOT NULL,
+  `Name` varchar(256) NOT NULL,
+  `instructor_id` varchar(20) NOT NULL,
+  `TA_id` varchar(20) DEFAULT NULL,
+  `introduction` varchar(256) DEFAULT NULL,
+  KEY `primarykey` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 正在導出表  pass.courses 的資料：~0 rows (大約)
+/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+
 -- 導出  表 pass.user 結構
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(20) NOT NULL,
@@ -25,11 +43,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   `role` varchar(256) NOT NULL DEFAULT '0',
   `email` varchar(256) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- 正在導出表  pass.user 的資料：~2 rows (大約)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `user_id`, `name`, `password`, `role`, `email`) VALUES
+REPLACE INTO `user` (`id`, `user_id`, `name`, `password`, `role`, `email`) VALUES
 	(1, '1', 'admin', '1', 'administrator', 'admin@gmail.com'),
 	(2, '123', 'Happy123', 'happy1234567890', 'instructor', 'happy@mail.com');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
